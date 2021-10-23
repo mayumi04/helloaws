a28299240b39d5ec6372c596fd3be03ccacbfca0
mayumi04 <83102566+mayumi04@users.noreply.github.com>
2021-10-23 11:20:13 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'helloaws',
+        'USER': 'admin',
+        'PASSWORD': 'ppppaaaa'
+        'HOST': 'db-helloaws.cjm4ozmphogl.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
